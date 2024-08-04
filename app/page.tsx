f5908cd9d003cbb034de10cdc32ac291b7bd3435
Goryachev Mikhail <m.goryachev@open.ru>
2024-08-04 23:11:33 +0300

    04-08-2024: Создание компонентов фильтров

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -7,6 +7,24 @@ export default function Home() {
         <Title text="Все товары" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+              {/* <ProductsGroupList title="" items={[1, 2, 3, 4, 5]} />
+              <ProductsGroupList title="" items={[1, 2, 3, 4, 5]} /> */}
+            </div>
+          </div>
+          
+        </div>
+      </Container>
     </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,5 +1,7 @@
 export { Categories } from './categories';
 export { Container } from './container';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
 export { Header } from './header';
 export { SortPopup } from './sort-popup';
 export { Title } from './title';
